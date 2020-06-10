a29d7c7ec26add621e04689a8d56badd83d78ad7
RamithR <ramithrandeniya@gmail.com>
2020-06-10 13:09:38 +0530

    updating fcm dependencies

--- app/src/main/java/com/ramith/ascentic/centicbids/utils/CBFirebaseInstanceIDService.kt
+++ app/src/main/java/com/ramith/ascentic/centicbids/utils/CBFirebaseInstanceIDService.kt
@@ -1,16 +1,17 @@
 package com.ramith.ascentic.centicbids.utils
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class CBFirebaseInstanceIDService : FirebaseMessagingService(){
 
-    override fun onNewToken(token : String?) {
-        super.onNewToken(token)
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
     }
 
-    override fun onMessageReceived(newMessage : RemoteMessage?) {
-        super.onMessageReceived(newMessage)
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
     }
 
 
